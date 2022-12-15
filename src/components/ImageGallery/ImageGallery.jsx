65c3aee83d7b7f9be4a31805c0f91258d6a936e4
Olga Birkovska <aiba.aki9@gmail.com>
2022-12-15 22:48:07 +0200

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { animateScroll } from 'react-scroll';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -10,91 +10,72 @@ import { Loader } from 'components/Loader/Loader';
 import { Button } from 'components/Button/Button';
 import { imageAPI } from 'services/image-api';
 
-export class ImageGallery extends Component {
-    static propTypes = {
-        data: PropTypes.string.isRequired,
-    };
+export const ImageGallery = ({ data }) => {
+    const [images, setImages] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [showButton, setShowButton] = useState(false);
+    const [pageNumber, setPageNumber] = useState(1);
 
-    state = {
-        images: null,
-        loading: false,
-        showButton: false,
-        pageNumber: 1,
-    };
+    useEffect(() => {
+        const fetchGallery = () => {
+            const page = 1;
+    
+            imageAPI
+                .fetchImages(data, page)
+                .then(images => {
+                    if (images.hits.length > 0 && images.totalHits <= 12) {
+                        setImages(images.hits);
+                        return;
+                    } else if (images.hits.length > 0 && images.totalHits > 12) {
+                        setImages(images.hits);
+                        setShowButton(true);
+                        setPageNumber(prevState => {
+                            return prevState + 1;
+                        });
+                        return;
+                    }
+                    toast.error('Oops! No matches found.');
+                })
+                .catch(error => toast.error(`${error.message}`))
+                .finally(() => setLoading(false));
+        };
+    
+        if (data === '') {
+            return;
+        }
 
-    componentDidUpdate(prevProps, prevState) {
-        if (prevProps.data !== this.props.data) {
-            this.setState({
-                loading: true,
-                images: null,
-                showButton: false,
-                pageNumber: 1,
-            });
-            this.fetchGallery();
-        } 
-    };
+        setLoading(true);
+        setImages(null);
+        setShowButton(false);
+        setPageNumber(1);
+        fetchGallery();
+    }, [data]);
 
-    fetchGallery = () => {
-        const query = this.props.data;
-        const page = 1;
-
-        imageAPI
-            .fetchImages(query, page)
-            .then(images => {
-                if (images.hits.length > 0 && images.totalHits <= 12) {
-                    this.setState({ images: images.hits });
-                    return;
-                } else if (images.hits.length > 0 && images.totalHits > 12) {
-                    this.setState(prevState => {
-                        return {
-                            images: images.hits,
-                            showButton: true,
-                            pageNumber: prevState.pageNumber + 1,
-                        };
-                    });
-                    return;
-                }
-                toast.error('Oops! No matches found.');
-            })
-            .catch(error => toast.error(`${error.message}`))
-            .finally(() => this.setState({ loading: false }));
-    };
-
-    loadMoreClick = () => {
-        this.setState({
-                loading: true,
-        });
-
-        const query = this.props.data;
-        const page = this.state.pageNumber;
+    const loadMoreClick = () => {
+        setLoading(true);
 
         imageAPI
-            .fetchImages(query, page)
+            .fetchImages(data, pageNumber)
             .then(images => {
                 if (images.hits.length > 0) {
                     animateScroll.scrollToBottom();
-                    this.setState(prevState => {
-                        return {
-                            images: [...prevState.images, ...images.hits],
-                            pageNumber: prevState.pageNumber + 1,
-                        }
-                    })
+                    setImages(prevState => {
+                        return [...prevState, ...images.hits];
+                    });
+                    setPageNumber(prevState => {
+                        return prevState + 1;
+                    });
                 }
                 
                 if (images.hits.length < 12) {
-                    this.setState({
-                        showButton: false,
-                        pageNumber: 1,
-                    });
+                    setShowButton(false);
+                    setPageNumber(1);
                     toast.info("Looks like you've reached the end of search results.");
                 }
             })
             .catch(error => toast.error(`${error.message}`))
-            .finally(() => this.setState({ loading: false }));
+            .finally(() => setLoading(false));
     }
-
-    render() {
-        const { images, loading, showButton } = this.state;
 
         return (
             <>
@@ -109,8 +90,12 @@ export class ImageGallery extends Component {
                     })}
                 </ul>
                 {loading && <Loader />}
-                {showButton && <Button onClick={this.loadMoreClick} />}
+                {showButton && <Button onClick={loadMoreClick} />}
             </>
         );
     };
+
+
+ImageGallery.propTypes = {
+    data: PropTypes.string.isRequired,
 };
