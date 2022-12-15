17a0422be3cb67e25b8540be33df8f15afb46082
Olga Birkovska <aiba.aki9@gmail.com>
2022-12-15 22:59:55 +0200

    Update image-api.js

--- src/services/image-api.js
+++ src/services/image-api.js
@@ -1,13 +1,15 @@
+import axios from 'axios';
+
 const BASE_URL = 'https://pixabay.com/api/'
 const API_KEY = '30551653-aa9d35c8f88064a7bc9ad69bf';
     
 async function fetchImages(query, pageNumber) {
     try {
         const fetchUrl = `${BASE_URL}?q=${query}&page=${pageNumber}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`;
-        const response = await fetch(fetchUrl);
+        const response = await axios.get(fetchUrl);
 
-        if (response.ok) {
-            return response.json();
+        if (response) {
+            return response.data;
         }
 
         return Promise.reject(
